69d0423da79cc75dbec9c4ab7c5e90537de39573
Amanda Samuelsson <AmandasMacbook@Amandas-MacBook.local>
2020-11-09 14:39:43 +0100

    Added soc med icons and slider

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
+window.addEventListener('load', main);
+
 const img = document.createElement("img");
 
 img.src = "logo.png";
 var src = document.getElementById("logo");
 
 src.appendChild(img);
+
+function main() {
+    startSlideshowAnimation();
+}
+
+function startSlideshowAnimation() {
+    setInterval(changeActiveImage, 5000)
+    changeActiveImage();
+}
+
+function changeActiveImage() {
+    const images = document.querySelectorAll('.slideshow-container > img');
+
+    for (let i = 0; i < images.length; i++) {
+
+        if (images[i].classList.contains('show')) {
+        images[i].classList.remove('show');
+
+        let nextIndex = i + 1;
+
+        if (nextIndex >=  images.length) {
+            nextIndex = 0; 
+        }
+
+        images[nextIndex].classList.add('show');
+
+        break;
+        }
+    }
+}
